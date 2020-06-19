d0d30e287ed87f54013aa89ad84e0c3f6261b5cc
Kasperice <kasper@alert.pl>
2020-06-19 07:47:00 +0200

    Exercise files

--- module1/solutions/exercise3.cpp
+++ module1/solutions/exercise3.cpp
@@ -1,21 +1,18 @@
+#include <memory>
 #include <string>
+#include <vector>
 
 class Ship {
-public:
-    Ship()
-        : id_(-1)
-    {}
+   public:
+    Ship() : id_(-1) {}
     Ship(int capacity, int maxCrew, int speed, const std::string& name, size_t id)
-        : capacity_(capacity)
-        , maxCrew_(maxCrew)
-        , crew_(0)
-        , speed_(speed)
-        , name_(name)
-        , id_(id)
-    {}
-    Ship(int maxCrew, int speed, size_t id)
-        : Ship(0, maxCrew, speed, "", id)
-    {}
+        : capacity_(capacity),
+          maxCrew_(maxCrew),
+          crew_(0),
+          speed_(speed),
+          name_(name),
+          id_(id) {}
+    Ship(int maxCrew, int speed, size_t id) : Ship(0, maxCrew, speed, "", id) {}
 
     void setName(const std::string& name) { name_ = name; }
 
@@ -28,17 +25,20 @@ public:
         return *this;
     }
 
-    size_t getCapacity() const  { return capacity_; }
-    size_t getMaxCrew() const   { return maxCrew_; }
-    size_t getSpeed() const     { return speed_; }
+    size_t getCapacity() const { return capacity_; }
+    size_t getMaxCrew() const { return maxCrew_; }
+    size_t getSpeed() const { return speed_; }
     std::string getName() const { return name_; }
-    size_t getId() const        { return id_; }
+    size_t getId() const { return id_; }
 
-private:
+    void load(std::shared_ptr<Cargo> cargo) { cargo_.push_back(cargo); }
+
+   private:
     size_t capacity_;
     size_t maxCrew_;
     size_t crew_;
     size_t speed_;
     std::string name_;
     const size_t id_;
+    std::vector<std::shared_ptr<Cargo>> cargo_;
 };
